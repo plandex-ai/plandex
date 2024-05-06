abbc8fcdae7da3c9bb953eaee8310fccf4c5293c
Dane Schneider <dane@envkey.com>
2024-05-06 11:26:12 -0700

    Fix for 'exit status 128' errors in a couple of edge case scenarios.

--- app/server/model/plan/tell_stream.go
+++ app/server/model/plan/tell_stream.go
@@ -1,6 +1,7 @@
 package plan
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -593,47 +594,82 @@ func (state *activeTellStreamState) onError(streamErr error, storeDesc bool, con
 		return
 	}
 
+	storeDescAndReply := func() {
+		ctx, cancelFn := context.WithCancel(context.Background())
+
+		repoLockId, err := db.LockRepo(
+			db.LockRepoParams{
+				UserId:   state.currentUserId,
+				OrgId:    state.currentOrgId,
+				PlanId:   planId,
+				Branch:   branch,
+				Scope:    db.LockScopeWrite,
+				Ctx:      ctx,
+				CancelFn: cancelFn,
+			},
+		)
+
+		if err != nil {
+			log.Printf("Error locking repo for plan %s: %v\n", planId, err)
+		} else {
+
+			defer func() {
+				err := db.UnlockRepo(repoLockId)
+				if err != nil {
+					log.Printf("Error unlocking repo for plan %s: %v\n", planId, err)
+				}
+			}()
+
+			err := db.GitClearUncommittedChanges(state.currentOrgId, planId)
+			if err != nil {
+				log.Printf("Error clearing uncommitted changes for plan %s: %v\n", planId, err)
+			}
+		}
+
+		storedMessage := false
+		storedDesc := false
+
+		if convoMessageId == "" {
+			assistantMsg, msg, err := state.storeAssistantReply()
+			if err == nil {
+				convoMessageId = assistantMsg.Id
+				commitMsg = msg
+				storedMessage = true
+			} else {
+				log.Printf("Error storing assistant message after stream error: %v\n", err)
+			}
+		}
+
+		if storeDesc && convoMessageId != "" {
+			err := db.StoreDescription(&db.ConvoMessageDescription{
+				OrgId:                 currentOrgId,
+				PlanId:                planId,
+				SummarizedToMessageId: summarizedToMessageId,
+				MadePlan:              false,
+				ConvoMessageId:        convoMessageId,
+				BuildPathsInvalidated: map[string]bool{},
+				Error:                 streamErr.Error(),
+			})
+			if err == nil {
+				storedDesc = true
+			} else {
+				log.Printf("Error storing description after stream error: %v\n", err)
+			}
+		}
+
+		if storedMessage || storedDesc {
+			err := db.GitAddAndCommit(currentOrgId, planId, branch, commitMsg)
+			if err != nil {
+				log.Printf("Error committing after stream error: %v\n", err)
+			}
+		}
+	}
+
+	storeDescAndReply()
+
 	active.StreamDoneCh <- &shared.ApiError{
 		Type:   shared.ApiErrorTypeOther,
 		Status: http.StatusInternalServerError,
 		Msg:    "Stream error: " + streamErr.Error(),
-	}
-
-	storedMessage := false
-	storedDesc := false
-
-	if convoMessageId == "" {
-		assistantMsg, msg, err := state.storeAssistantReply()
-		if err == nil {
-			convoMessageId = assistantMsg.Id
-			commitMsg = msg
-			storedMessage = true
-		} else {
-			log.Printf("Error storing assistant message after stream error: %v\n", err)
-		}
-	}
-
-	if storeDesc && convoMessageId != "" {
-		err := db.StoreDescription(&db.ConvoMessageDescription{
-			OrgId:                 currentOrgId,
-			PlanId:                planId,
-			SummarizedToMessageId: summarizedToMessageId,
-			MadePlan:              false,
-			ConvoMessageId:        convoMessageId,
-			BuildPathsInvalidated: map[string]bool{},
-			Error:                 streamErr.Error(),
-		})
-		if err == nil {
-			storedDesc = true
-		} else {
-			log.Printf("Error storing description after stream error: %v\n", err)
-		}
-	}
-
-	if storedMessage || storedDesc {
-		err := db.GitAddAndCommit(currentOrgId, planId, branch, commitMsg)
-		if err != nil {
-			log.Printf("Error committing after stream error: %v\n", err)
-		}
 	}
 }
