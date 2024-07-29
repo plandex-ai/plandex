4aa73a855806e4a642a32d7408f3c698b93522d4
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2024-07-29 18:51:54 +0100

    chore: finish java code for verify

--- test/evals/promptfoo-poc/verify/assets/java/code/start.java
+++ test/evals/promptfoo-poc/verify/assets/java/code/start.java
@@ -1,6 +1,6 @@
-pdx-1: import java.util.*;
-pdx-2: public class Example {
-pdx-3:     public static void main(String[] args) {
-pdx-4:         System.out.println("Starting application...");
-pdx-5:     }
-pdx-6: }
+pdx-1: pdx-1: import java.util.*;
+pdx-2: pdx-2: public class Example {
+pdx-3: pdx-3:     public static void main(String[] args) {
+pdx-4: pdx-4:         System.out.println("Starting application...");
+pdx-5: pdx-5:     }
+pdx-6: pdx-6: }
