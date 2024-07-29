836264394765abce8fb77bdea82a0043cde492ac
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2024-07-29 18:53:31 +0100

    chore: Refactor Java code and fix TODO comments

--- test/evals/promptfoo-poc/verify/assets/java/code/calculator.java
+++ test/evals/promptfoo-poc/verify/assets/java/code/calculator.java
@@ -1,9 +1,9 @@
-pdx-1: pdx-1: class Calculator {
-pdx-2: pdx-2: 	private int result = 0;
-pdx-3: pdx-3: 
-pdx-4: pdx-4: 	public void add(int number) {
-pdx-5: pdx-5: 		result += number;
-pdx-6: pdx-6: 	}
-pdx-7: pdx-7: 
-pdx-8: pdx-8: 	// TODO: Subtract method
-pdx-9: pdx-9: }
+pdx-1: class Calculator {
+pdx-2: 	private int result = 0;
+pdx-3: 
+pdx-4: 	public void add(int number) {
+pdx-5: 		result += number;
+pdx-6: 	}
+pdx-7: 
+pdx-8: 	// TODO: Subtract method
+pdx-9: }
--- test/evals/promptfoo-poc/verify/assets/java/code/solution.java
+++ test/evals/promptfoo-poc/verify/assets/java/code/solution.java
@@ -1,10 +1,10 @@
-pdx-1: pdx-1: import java.util.*;
-pdx-2: pdx-2: 
-pdx-3: pdx-3: class Solution {
-pdx-4: pdx-4:     public static void main(String[] args) {
-pdx-5: pdx-5:         System.out.println("Array before sort: " + Arrays.toString(args));
-pdx-6: pdx-6:         Arrays.sort(args);
-pdx-7: pdx-7:         System.out.println("Array after sort: " + Arrays.toString(args));
-pdx-8: pdx-8:     }
-pdx-9: pdx-9: }
-pdx-10: pdx-10:  
+pdx-1: import java.util.*;
+pdx-2: 
+pdx-3: class Solution {
+pdx-4:     public static void main(String[] args) {
+pdx-5:         System.out.println("Array before sort: " + Arrays.toString(args));
+pdx-6:         Arrays.sort(args);
+pdx-7:         System.out.println("Array after sort: " + Arrays.toString(args));
+pdx-8:     }
+pdx-9: }
+pdx-10:   
