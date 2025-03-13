90086a04201c30c6af9618e524b0a624a9618d01
Dane Schneider <dane@plandex.ai>
2025-03-13 10:16:38 -0700

    migration to fix creation of model packs #211

---
+++ app/server/migrations/2025031300_add_model_roles.down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE model_sets DROP COLUMN context_loader;
+ALTER TABLE model_sets DROP COLUMN whole_file_builder;
+ALTER TABLE model_sets DROP COLUMN coder;
---
+++ app/server/migrations/2025031300_add_model_roles.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE model_sets ADD COLUMN context_loader JSON;
+ALTER TABLE model_sets ADD COLUMN whole_file_builder JSON;
+ALTER TABLE model_sets ADD COLUMN coder JSON;
